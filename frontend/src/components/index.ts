cd5460097acbdaa4a3eb5aaac5b277dcf4c1c7bf
nasviil <itsfrnasviil@gmail.com>
2025-09-28 19:38:32 +0800

    add index file to export components for EmployeeTable, TaxBracketsTable, AddEmployeeModal, and TaxCalculatorModal

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as EmployeeTable } from './EmployeeTable';
+export { default as TaxBracketsTable } from './TaxBracketsTable';
+export { default as AddEmployeeModal } from './AddEmployeeModal';
+export { default as TaxCalculatorModal } from './TaxCalculatorModal';
