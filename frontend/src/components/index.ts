3c13581bbede3cf6f10cfff02c014eb0f8f79235
nasviil <itsfrnasviil@gmail.com>
2025-09-30 02:14:16 +0800

    feat: add EditEmployeeModal export to component index

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as EmployeeTable } from './EmployeeTable';
 export { default as TaxBracketsTable } from './TaxBracketsTable';
 export { default as AddEmployeeModal } from './AddEmployeeModal';
+export { default as EditEmployeeModal } from './EditEmployeeModal';
 export { default as TaxCalculatorModal } from './TaxCalculatorModal';
