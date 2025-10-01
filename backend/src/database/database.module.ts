b72e75479396538532ff47bd4df067010199de62
nasviil <itsfrnasviil@gmail.com>
2025-10-01 23:20:04 +0800

    feat: add EmployeeSeeder and TaxService to DatabaseModule providers and exports

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -4,10 +4,12 @@ import { TaxBracket } from '../entities/tax-bracket.entity';
 import { Employee } from '../entities/employee.entity';
 import { DatabaseService } from './database.service';
 import { TaxBracketSeeder } from './seeds/tax-bracket.seeder';
+import { EmployeeSeeder } from './seeds/employee.seeder';
+import { TaxService } from '../services/tax.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TaxBracket, Employee])],
-  providers: [DatabaseService, TaxBracketSeeder],
-  exports: [DatabaseService, TaxBracketSeeder],
+  providers: [DatabaseService, TaxBracketSeeder, EmployeeSeeder, TaxService],
+  exports: [DatabaseService, TaxBracketSeeder, EmployeeSeeder, TaxService],
 })
 export class DatabaseModule {}
