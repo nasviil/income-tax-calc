9a9683af909cb7761c18ad0966c539ed637c4edc
nasviil <itsfrnasviil@gmail.com>
2025-10-01 22:44:59 +0800

    feat: remove TaxCalculatorModal from component exports to simplify UI

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,5 +2,4 @@ export { default as EmployeeTable } from './EmployeeTable';
 export { default as TaxBracketsTable } from './TaxBracketsTable';
 export { default as AddEmployeeModal } from './AddEmployeeModal';
 export { default as EditEmployeeModal } from './EditEmployeeModal';
-export { default as TaxCalculatorModal } from './TaxCalculatorModal';
 export { default as TaxBracketsModal } from './TaxBracketsModal';
